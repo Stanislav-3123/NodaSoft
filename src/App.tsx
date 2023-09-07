6e374abc5990a8b79dc66f9be7360060bf41fb06
Stanislav-3123 <pshedzial.stas@gmail.com>
2023-09-07 15:24:10 +0300

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useMemo } from "react";
 
-function App() {
+const URL = "https://jsonplaceholder.typicode.com/users";
+
+type Company = {
+  bs: string;
+  catchPhrase: string;
+  name: string;
+};
+
+type Address = {
+  // Укажите здесь все необходимые поля и их типы
+};
+
+type User = {
+  id: number;
+  email: string;
+  name: string;
+  phone: string;
+  username: string;
+  website: string;
+  company: Company;
+  address: Address;
+};
+
+interface IButtonProps {
+  onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+}
+
+function Button({ onClick }: IButtonProps): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button type="button" onClick={onClick}>
+      get random user
+    </button>
+  );
+}
+
+interface IUserInfoProps {
+  user: User | null;
+}
+
+function UserInfo({ user }: IUserInfoProps): JSX.Element | null {
+  if (!user) return null;
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Username</th>
+          <th>Phone number</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{user.name}</td>
+          <td>{user.phone}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+const UserInfoMemo = React.memo(UserInfo);
+
+function useThrottle(callback: () => void, delay: number): (() => void) {
+  const [timerId, setTimerId] = useState<number | null>(null);
+
+  return () => {
+    if (timerId) return;
+    callback();
+    setTimerId(setTimeout(() => setTimerId(null), delay) as unknown as number);
+  };
+}
+
+function App(): JSX.Element {
+  const [user, setUser] = useState<User | null>(null);
+  const cache: Record<number, User> = useMemo(() => ({}), []);
+
+  const receiveRandomUser = useCallback(async () => {
+    try {
+      const id = Math.floor(Math.random() * (10 - 1)) + 1;
+      if (user && user.id === id) return;
+      if (cache[id]) {
+        setUser(cache[id]);
+        return;
+      }
+      const response = await fetch(`${URL}/${id}`);
+      const _user = (await response.json()) as User;
+      cache[id] = _user;
+      setUser(_user);
+    } catch (error) {
+      console.error("Ошибка при получении данных:", error);
+    }
+  }, [user, cache]);
+
+  const handleButtonClick = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      event.stopPropagation();
+      receiveRandomUser();
+    },
+    [receiveRandomUser]
+  );
+
+  const throttledHandleButtonClick = useThrottle(handleButtonClick, 500);
+
+  return (
+    <div>
+      <header>Get a random user</header>
+      <Button onClick={throttledHandleButtonClick} />
+      <UserInfoMemo user={user} />
     </div>
   );
 }
 
 export default App;
+
